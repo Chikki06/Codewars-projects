a58d7c6e8fbba05f05d94da3ba5cc71144043970
Chikki06 <72044286+Chikki06@users.noreply.github.com>
2022-03-07 09:45:35 +0530

    filling the code

---
+++ Find-the-parity-outlier-(6kyu).py
@@ -0,0 +1,33 @@
+#Problem Statment
+# You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+#The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+#Write a method that takes the array as an argument and returns this "outlier" N.
+#
+# Examples
+# [2, 4, 0, 100, 4, 11, 2602, 36]
+# Should return: 11 (the only odd number)
+#
+# [160, 3, 1719, 19, 11, 13, -21]
+# Should return: 160 (the only even number)
+
+#Code
+
+def find_outlier(integers):
+    if equal(integers[0], integers[len(integers)-1]) == True:
+        return(func(integers[0]%2,integers))
+    elif equal(integers[1], integers[2]) == True:
+        return(func(integers[2]%2, integers))
+def func(x, integers):
+    if x == 1:
+        for m in range(len(integers)):
+            if int(integers[m])%2 == 0:
+                return integers[m]
+    elif x == 0:
+        for m in range(len(integers)):
+            if int(integers[m])%2 == 1:
+                return integers[m]
+def equal(x,y):
+    if x%2 == y%2:
+        return True
+    else:
+        return False
---
+++ Who-likes-it-(6kyu).py
@@ -0,0 +1,26 @@
+#Problem statement
+# You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+#
+# Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+#
+# []                                -->  "no one likes this"
+# ["Peter"]                         -->  "Peter likes this"
+# ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+# ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+# ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+# Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+#Code
+
+def likes(names):
+    if len(names) == 0:
+        return ("no one likes this")
+    elif len(names)==1:
+        return (str(names[0])+" likes this")
+    elif len(names)==2:
+        return (str(names[0])+" and "+str(names[1])+" like this")
+    elif len(names)==3:
+        return (str(names[0])+", "+ str(names[1])+" and " + str(names[2]) + " like this")
+    elif len(names) > 3:
+        num = len(names) -2
+        return (str(names[0])+", "+str(names[1])+" and "+ str(num)+ " others like this")
